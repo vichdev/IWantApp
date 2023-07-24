e0cde49c513ce662ce99ef73a33050dc6de18362
VictorHugo <victor.hugo.antunes.n@gmail.com>
2023-07-24 10:29:04 -0300

    feat: criação de primeira tabela e conexão

--- IWantApp/Domain/Products/Product.cs
+++ IWantApp/Domain/Products/Product.cs
@@ -6,4 +6,5 @@ public class Product : Entity
     public Guid CategoryId { get; set; }
     public Category Category { get; set; }
     public bool HasStock { get; set; }
+    public string teste { get; set; }
 }
